698f4a6c702470787cb82d3f245dc76fa47b8318
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-06-07 21:09:22 -0300

    Adiciona o componente MyProvider na função App e o componente Table como filho

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './App.css';
+import MyProvider from './context/MyProvider';
+import Table from './components/Table';
 
 function App() {
   return (
-    <h1>Hello, App!</h1>
+    <MyProvider>
+      <Table />
+    </MyProvider>
   );
 }
 
