b7a19764968a06169a535ef9b51c97c84b403916
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-06-08 02:48:10 -0300

    Adiciona o componente Filter para manipulação da tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,48 +1,51 @@
 import React, { useContext } from 'react';
 import MyContext from '../context/MyContext';
+import Filter from './Filter';
 
 function Table() {
-  const { resultsApi } = useContext(MyContext);
+  const { searchFilter } = useContext(MyContext);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>climate</th>
-          <th>created</th>
-          <th>diameter</th>
-          <th>edited</th>
-          <th>films</th>
-          <th>gravity</th>
-          <th>name</th>
-          <th>orbital_period</th>
-          <th>population</th>
-          <th>residents</th>
-          <th>rotation_period</th>
-          <th>surface_water</th>
-          <th>terrain</th>
-        </tr>
-      </thead>
-      <tbody>
-        {resultsApi.map((result) => (
-          <tr key={ result.name }>
-            <td>{result.climate}</td>
-            <td>{result.created}</td>
-            <td>{result.diameter}</td>
-            <td>{result.edited}</td>
-            <td>{result.films}</td>
-            <td>{result.gravity}</td>
-            <td>{result.name}</td>
-            <td>{result.orbital_period}</td>
-            <td>{result.population}</td>
-            <td>{result.residents}</td>
-            <td>{result.rotation_period}</td>
-            <td>{result.surface_water}</td>
-            <td>{result.terrain}</td>
+    <>
+      <Filter />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Url</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {searchFilter.map((result) => (
+            <tr key={ result.name }>
+              <td>{result.name}</td>
+              <td>{result.rotation_period}</td>
+              <td>{result.orbital_period}</td>
+              <td>{result.diameter}</td>
+              <td>{result.climate}</td>
+              <td>{result.gravity}</td>
+              <td>{result.terrain}</td>
+              <td>{result.surface_water}</td>
+              <td>{result.population}</td>
+              <td>{result.films}</td>
+              <td>{result.created}</td>
+              <td>{result.edited}</td>
+              <td>{result.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
